11d045d62fae0c515397df525be488ce3dbcb104
Miguel Angel Guarin <mago123005@gmail.com>
2025-08-15 16:55:46 -0500

    Adicion del reto en clase

---
+++ reto_clase1.py
@@ -0,0 +1,21 @@
+numerosuma1 = 10
+numerosuma2 = 20
+resultadosuma = 10 + 20
+
+numeroresta1 = 5
+numeroresta2 = 10
+resultadoresta = 5 - 10
+
+numerodivision1 = 10
+numerodivision2 = 5
+resultadodivision = 10 / 5
+
+numeromultiplicacion1 = 5
+numeromultplicacion2 = 5
+resultadomultiplicacion = 5 * 5
+
+print("Hoy en clase tuve una clase de matematicas bien buena...")
+print("La suma fue", numerosuma1, "y", numerosuma2, "dandome el resultado de", resultadosuma)
+print("La resta fue", numeroresta1, "y", numeroresta2, "dandome el resultado de", resultadoresta)
+print("La multiplicacion fue", numeromultiplicacion1, "y", numeromultplicacion2, "dandome el resultado de", resultadomultiplicacion)
+print("La division fue", numerodivision1, "y", numerodivision2, "dandome el resultado de", resultadodivision)
