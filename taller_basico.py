7826cef181b9480c24fb4e1b1ad9349ff3eb04ac
Miguel Angel Guarin <mago123005@gmail.com>
2025-08-15 16:40:39 -0500

    segunda clase electiva 2.0

---
+++ taller_basico.py
@@ -0,0 +1,6 @@
+nombre = "Miguel Angel Guarin Ospina"
+edad = "19"
+ciudad = "Armenia Quindio"
+hobby = "leer comics"
+
+print("Me llamo", nombre, "tengo" , edad, "años y vivo en", ciudad,"me gusta", hobby )
